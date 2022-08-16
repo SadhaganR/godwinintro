a57e6a344789665275878245d4312c295667d9be
“sadhaganr” <r.sadhagan@gmail.com>
2022-08-16 07:42:45 +0000

    second commit tonny 16aug2022

---
+++ selfintro2.java
@@ -0,0 +1,16 @@
+hi i am sathagan
+i completed my dipoloma in the yr 2010
+after completing my education i have aroud 12 yrs of job experiance.
+5yrs as telecaller & 7yrs of experiance in IT sector
+in begining of my IT carrier first 3yrs worked as desktop support , 1yr as field engg & last 3yrs as technical support
+in my resume i have listed out all companies that i worked so far.'
+
+
+Name: Sathagan
+Mobile: 9585707873
+E-mail: r.sadhagan@gmail.com
+Total IT Exp: 7yrs
+Passedout: 2010
+Last CTC: 2.8
+Native: Bodinayakannur , theni dst
+Completed AWS&Devops: Dec – Feb Greenstechnology OMR Expected CTC: 6-8 L
